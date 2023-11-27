26dad28de57db1b52ad145b882087dcb9b35d796
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-27 20:09:44 +0100

    added ciclo for , conditions and reset

--- js/script.js
+++ js/script.js
@@ -2,20 +2,55 @@ const button = document.querySelector('button');
 
 const mainContentEl=document.querySelector('main section.container');
 
+let turnCounter=0;
+
+const numberBombs = 16;
+
+const numberCells = 100;
+
+//SCORE//
+const scoreboardEl = document.getElementById('score');
+let userScore = 0;
+
+let isGameOver = false;
+
 //click event//
 button.addEventListener("click", function() {
 
     mainContentEl.innerHTML='';
 
-    for(let i = 1; i <= 100; i++){
-        const newSquare = createSquare();
-        mainContentEl.appendChild(newSquare);
+    const bombs = [];
+    while( bombs.length < numberBombs){
+        let randomNumber = getRandomNumber(1, numberCells)
+        if ( !bombs.includes(randomNumber) ){
+            bombs.push(randomNumber);
+        }
+    }
+    console.log(bombs)
 
+    for(let i = 1; i <= numberCells ; i++){
+        const newSquare = createSquare();  
+        mainContentEl.appendChild(newSquare);
+        
         newSquare.addEventListener('click',function(){
             const contentSquare = i;
             newSquare.innerHTML = contentSquare;
-            newSquare.classList.toggle('clicked-blue'); 
-            console.log(contentSquare);
+            
+           // newSquare.classList.toggle('clicked-blue'); 
+            //console.log(contentSquare);
+
+            if (bombs.includes(contentSquare)){
+                newSquare.classList.toggle('bg-red');
+                resetGame();
+                
+            } else {
+                newSquare.classList.toggle('bg-blue');
+                turnCounter++;
+                if( turnCounter == (numberCells - numberBombs)){
+                    winGame()
+                }
+            }
+            
         }
 )}
 })
@@ -30,9 +65,36 @@ newSquareEl.classList.add('flex-aj-cent');
 return newSquareEl
 }
 
-
 //RANDOM NUMBER//
 function getRandomNumber(minNumber, maxNumber){
     return Math.floor( Math.random() *(maxNumber - minNumber + 1) + minNumber);
 }
 
+//SCORE//
+function updateCurrentScore(scoreToUpdate){
+    scoreboardEl.innerText = scoreToUpdate;
+}
+
+//CELLE BOMBE//
+function showBombCells(){
+    const bombElements = document.querySelectorAll('article.bg-red');
+    for (let index = 0; index < bombElements.length; index++) {
+        const bombElement = bombElements[index];
+        bombElement.classList.add('clicked');
+        bombElement.innerHTML = `<span> <i class="fa-solid fa-bomb fa-beat"></i> </span>`;
+    }
+}
+
+//END GAME//
+function resetGame (){
+    userScore = 0;
+    turnCounter=0
+    isGameOver = false;
+    updateCurrentScore(userScore);
+    
+}
+
+//WIN//
+function winGame (){
+    resetGame();
+}
